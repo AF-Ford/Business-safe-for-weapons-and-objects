c9778fa842bb15a356fccda545142155d7dc34b9
[DEV] N.O.S <54995283+AslanDoblow@users.noreply.github.com>
2019-10-31 16:22:18 +0100

    Add files via upload

---
+++ coffre_entreprise/config.lua
@@ -0,0 +1,24 @@
+Config                            = {}
+
+Config.DrawDistance               = 100.0
+Config.MarkerType                 = 1
+Config.MarkerSize                 = { x = 1.5, y = 1.5, z = 0.5 }
+Config.MarkerColor                = { r = 50, g = 50, b = 204 }
+Config.EnableArmoryManagement     = true
+
+Config.Locale                     = 'fr'
+Config.vigneStations = {
+
+	LSPD = {
+
+
+		Armories = {
+			vector3(-137.04, -1609.9, 35.03)
+		}
+
+	
+
+	}
+
+}
+
